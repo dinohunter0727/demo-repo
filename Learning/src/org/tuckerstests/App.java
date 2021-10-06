75f83239384a4dabcc8a6ed9079988eca6f7b56a
tucke <tucke@10.0.0.116>
2021-10-05 21:58:25 -0600

    initial commit!

---
+++ Learning/src/org/tuckerstests/App.java
@@ -0,0 +1,18 @@
+package org.tuckerstests;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class App {
+
+	public static void main(String[] args) throws IOException {
+		System.out.println("Hello GIT Test!");
+	}
+}
+
