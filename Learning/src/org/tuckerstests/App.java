004356e23e00b9b5b7edac1b4dfa0338a11f6660
tucke <tucke@10.0.0.116>
2021-10-05 22:12:30 -0600

    Ahas

--- Learning/src/org/tuckerstests/App.java
+++ Learning/src/org/tuckerstests/App.java
@@ -12,7 +12,7 @@ import java.util.Scanner;
 public class App {
 
 	public static void main(String[] args) throws IOException {
-		System.out.println("Hello GIT Test!");
+		System.out.println("Hello GIT sTest!");
 	}
 }
 
